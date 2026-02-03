79d2b8f01e489c655cef2ee82aa66c91e32a06d4
emonmahdi <developeremon1997@gmail.com>
2026-02-03 16:07:14 +0600

    sidebar are done

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
-import { SIDEBAR_CLASSES, PRODUCTIVITY_CARD } from "../assets/dummy";
-import { Sparkles } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router";
+import {
+  SIDEBAR_CLASSES,
+  PRODUCTIVITY_CARD,
+  menuItems,
+  LINK_CLASSES,
+  TIP_CARD,
+} from "../assets/dummy";
+import { Sparkles, Lightbulb, Menu, X } from "lucide-react";
 
 const Sidebar = ({ user, tasks }) => {
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -14,6 +21,43 @@ const Sidebar = ({ user, tasks }) => {
 
   const username = user?.name || "User";
   const initial = username.charAt(0).toUpperCase();
+
+  useEffect(() => {
+    document.body.style.overflow = mobileOpen ? "hidden" : "auto";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [mobileOpen]);
+
+  const renderMenuItems = (isMobile = false) => (
+    <ul className="space-y-2">
+      {menuItems.map(({ text, path, icon }) => (
+        <li key={text}>
+          <NavLink
+            to={path}
+            className={({ isActive }) =>
+              [
+                LINK_CLASSES.base,
+                isActive ? LINK_CLASSES.active : LINK_CLASSES.inactive,
+                isMobile ? "justify-start" : "justify-start",
+              ].join(" ")
+            }
+            onClick={() => setMobileOpen(false)}
+          >
+            <span className={LINK_CLASSES.icon}>{icon}</span>
+            <span
+              className={`${
+                isMobile ? "block" : "hidden lg:block"
+              } ${LINK_CLASSES} ml-1`}
+            >
+              {text}
+            </span>
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
     <>
@@ -48,8 +92,82 @@ const Sidebar = ({ user, tasks }) => {
               />
             </div>
           </div>
+          {renderMenuItems()}
+          <div className="mt-auto pt-6 lg:block hidden">
+            <div className={TIP_CARD.container}>
+              <div className="flex items-center gap-2">
+                <div className={TIP_CARD.iconWrapper}>
+                  <Lightbulb className="w-5 h-5 text-purple-600" />
+                </div>
+
+                <div>
+                  <h3 className={TIP_CARD.title}>Pro Tip</h3>
+                  <p className={TIP_CARD.text}>
+                    Use keyboard shortcuts to boost productivity!
+                  </p>
+                  <a
+                    href="https://devemonmahdi.netlify.app/"
+                    target="_blank"
+                    className="block mt-2 text-sm text-purple-500 hover:underline"
+                  >
+                    Visit Site - Emon Madhi
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      {/* MOBILE MENU */}
+      {!mobileOpen && (
+        <button
+          onClick={() => setMobileOpen(true)}
+          className={SIDEBAR_CLASSES.mobileButton}
+        >
+          <Menu className="w-5 h-5" />
+        </button>
+      )}
+
+      {/* MOBILE DRAWER */}
+      {mobileOpen && (
+        <div className="fixed inset-0 z-40">
+          <div
+            className={SIDEBAR_CLASSES.mobileDrawerBackdrop}
+            onClick={() => setMobileOpen(false)}
+          />
+
+          <div
+            className={SIDEBAR_CLASSES.mobileDrawer}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex justify-between items-center mb-4 border-b pb-2">
+              <h2 className="text-lg font-bold text-purple-600">Menu</h2>
+              <button
+                onClick={() => setMobileOpen(false)}
+                className="text-gray-700 hover:text-purple-600"
+              >
+                <X className="w-5 h-5" />
+              </button>
+            </div>
+
+            <div className="flex items-center gap-3 mb-6">
+              <div className="w-10 h-10 mt-16 rounded-full bg-gradient-to-br from-fuchsia-500 to-purple-600 flex items-center justify-center text-white font-bold shadow-md">
+                {initial}
+              </div>
+              <div>
+                <h2 className="text-lg mt-16 font-bold text-gray-800">
+                  Hey, {username}
+                </h2>
+                <p className="text-sm text-purple-500 font-medium flex items-center gap-1">
+                  <Sparkles className="w-3 h-3" />
+                  Let’s crush some tasks!
+                </p>
+              </div>
+            </div>
+            {renderMenuItems(true)}
+          </div>
+        </div>
+      )}
     </>
   );
 };
